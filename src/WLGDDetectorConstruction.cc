b84c82360925dd4010ce806760c14d61b2e0fbc5
yramachers <yramachers@users.noreply.github.com>
2020-06-05 15:39:58 +0100

    Converge geom (#21)
    * [geometry] factorized materials definition out of volume construction.
    
    * [biasing] attach biasing to all logical volumes of interest.
    
    * [detector] took out mu- from particle filter for SD. Scores from mu- in Ge are of no interest.
    
    * [format] clang format.

--- src/WLGDDetectorConstruction.cc
+++ src/WLGDDetectorConstruction.cc
@@ -33,9 +33,13 @@
 
 #include "G4PhysicalConstants.hh"
 #include "G4SystemOfUnits.hh"
-#include "G4UserLimits.hh"
 
-WLGDDetectorConstruction::WLGDDetectorConstruction() { DefineCommands(); }
+WLGDDetectorConstruction::WLGDDetectorConstruction()
+{
+  DefineCommands();
+
+  DefineMaterials();
+}
 
 WLGDDetectorConstruction::~WLGDDetectorConstruction() { delete fDetectorMessenger; }
 
@@ -55,84 +59,15 @@ auto WLGDDetectorConstruction::Construct() -> G4VPhysicalVolume*
   return SetupAlternative();
 }
 
-void WLGDDetectorConstruction::ConstructSDandField()
+void WLGDDetectorConstruction::DefineMaterials()
 {
-  G4SDManager::GetSDMpointer()->SetVerboseLevel(2);
-
-  // Only need to construct the (per-thread) SD once
-  if(!fSD.Get())
-  {
-    auto* det = new G4MultiFunctionalDetector("Det");
-    fSD.Put(det);
-
-    auto* vertexFilter = new G4SDParticleFilter("vtxfilt");
-    vertexFilter->add("neutron");  // neutrons in Ge of interest
-    vertexFilter->add("mu-");      // muons in Ge of no interest
-    vertexFilter->addIon(32, 77);  // register 77Ge production
-
-    auto* eprimitive = new WLGDPSEnergyDeposit("Edep");
-    eprimitive->SetFilter(vertexFilter);
-    det->RegisterPrimitive(eprimitive);
-
-    auto* tprimitive = new WLGDPSTime("Time");
-    tprimitive->SetFilter(vertexFilter);
-    det->RegisterPrimitive(tprimitive);
-
-    auto* lprimitive = new WLGDPSLocation("Loc");
-    lprimitive->SetFilter(vertexFilter);
-    det->RegisterPrimitive(lprimitive);
-
-    auto* idprimitive = new WLGDPSTrackID("TrackID");
-    idprimitive->SetFilter(vertexFilter);
-    det->RegisterPrimitive(idprimitive);
-
-    auto* primitive = new WLGDPSParentID("ParentID");
-    primitive->SetFilter(vertexFilter);
-    det->RegisterPrimitive(primitive);
-
-    // Also only add it once to the SD manager!
-    G4SDManager::GetSDMpointer()->AddNewDetector(fSD.Get());
-  }
-
-  SetSensitiveDetector("Ge_log", fSD.Get());
-
-  // ----------------------------------------------
-  // -- operator creation and attachment to volume:
-  // ----------------------------------------------
-  G4LogicalVolumeStore* volumeStore = G4LogicalVolumeStore::GetInstance();
-
-  // -- Attach neutron XS biasing to required volumes
-  auto* biasnXS = new WLGDBiasMultiParticleChangeCrossSection();
-  biasnXS->AddParticle("neutron");
-  G4LogicalVolume* logicGe = volumeStore->GetVolume("Ge_log");
-  biasnXS->AttachTo(logicGe);
-
-  // -- Attach muon XS biasing to required volumes
-  auto* biasmuXS = new WLGDBiasMultiParticleChangeCrossSection();
-  biasmuXS->AddParticle("mu-");
-
-  G4LogicalVolume* logicLar = volumeStore->GetVolume("Lar_log");
-  biasmuXS->AttachTo(logicLar);
-
-  G4LogicalVolume* logicULar = volumeStore->GetVolume("ULar_log");
-  biasmuXS->AttachTo(logicULar);
-
-  // Baseline also has a water volume
-  if(fGeometryName == "baseline")
-  {
-    G4LogicalVolume* logicWater = volumeStore->GetVolume("Water_log");
-    biasmuXS->AttachTo(logicWater);
-  }
-}
-
-auto WLGDDetectorConstruction::SetupAlternative() -> G4VPhysicalVolume*
-{
-  G4NistManager* nistManager   = G4NistManager::Instance();
-  G4Material*    worldMaterial = nistManager->FindOrBuildMaterial("G4_Galactic");
-  G4Material*    larMat        = nistManager->FindOrBuildMaterial("G4_lAr");
-  G4Material*    airMat        = nistManager->FindOrBuildMaterial("G4_AIR");
-  G4Material*    steelMat      = nistManager->FindOrBuildMaterial("G4_STAINLESS-STEEL");
-  G4Material*    copperMat     = nistManager->FindOrBuildMaterial("G4_Cu");
+  G4NistManager* nistManager = G4NistManager::Instance();
+  nistManager->FindOrBuildMaterial("G4_Galactic");
+  nistManager->FindOrBuildMaterial("G4_lAr");
+  nistManager->FindOrBuildMaterial("G4_AIR");
+  nistManager->FindOrBuildMaterial("G4_STAINLESS-STEEL");
+  nistManager->FindOrBuildMaterial("G4_Cu");
+  nistManager->FindOrBuildMaterial("G4_WATER");
 
   auto* C  = new G4Element("Carbon", "C", 6., 12.011 * g / mole);
   auto* O  = new G4Element("Oxygen", "O", 8., 16.00 * g / mole);
@@ -166,6 +101,111 @@ auto WLGDDetectorConstruction::SetupAlternative() -> G4VPhysicalVolume*
   G4double density = 3.323 * mg / cm3;
   auto*    roiMat  = new G4Material("enrGe", density, 1);
   roiMat->AddElement(eGe, 1);
+}
+
+void WLGDDetectorConstruction::ConstructSDandField()
+{
+  G4SDManager::GetSDMpointer()->SetVerboseLevel(2);
+
+  // Only need to construct the (per-thread) SD once
+  if(!fSD.Get())
+  {
+    auto* det = new G4MultiFunctionalDetector("Det");
+    fSD.Put(det);
+
+    auto* vertexFilter = new G4SDParticleFilter("vtxfilt");
+    vertexFilter->add("neutron");  // neutrons in Ge of interest
+    vertexFilter->addIon(32, 77);  // register 77Ge production
+
+    auto* eprimitive = new WLGDPSEnergyDeposit("Edep");
+    eprimitive->SetFilter(vertexFilter);
+    det->RegisterPrimitive(eprimitive);
+
+    auto* tprimitive = new WLGDPSTime("Time");
+    tprimitive->SetFilter(vertexFilter);
+    det->RegisterPrimitive(tprimitive);
+
+    auto* lprimitive = new WLGDPSLocation("Loc");
+    lprimitive->SetFilter(vertexFilter);
+    det->RegisterPrimitive(lprimitive);
+
+    auto* idprimitive = new WLGDPSTrackID("TrackID");
+    idprimitive->SetFilter(vertexFilter);
+    det->RegisterPrimitive(idprimitive);
+
+    auto* primitive = new WLGDPSParentID("ParentID");
+    primitive->SetFilter(vertexFilter);
+    det->RegisterPrimitive(primitive);
+
+    // Also only add it once to the SD manager!
+    G4SDManager::GetSDMpointer()->AddNewDetector(fSD.Get());
+  }
+
+  SetSensitiveDetector("Ge_log", fSD.Get());
+
+  // ----------------------------------------------
+  // -- operator creation and attachment to volume:
+  // ----------------------------------------------
+  G4LogicalVolumeStore* volumeStore = G4LogicalVolumeStore::GetInstance();
+
+  // -- Attach neutron XS biasing to Germanium -> enhance nCapture
+  auto* biasnXS = new WLGDBiasMultiParticleChangeCrossSection();
+  biasnXS->AddParticle("neutron");
+  G4LogicalVolume* logicGe = volumeStore->GetVolume("Ge_log");
+  biasnXS->AttachTo(logicGe);
+
+  // -- Attach muon XS biasing to all required volumes consistently
+  auto* biasmuXS = new WLGDBiasMultiParticleChangeCrossSection();
+  biasmuXS->AddParticle("mu-");
+
+  G4LogicalVolume* logicCavern = volumeStore->GetVolume("Cavern_log");
+  biasmuXS->AttachTo(logicCavern);
+  G4LogicalVolume* logicHall = volumeStore->GetVolume("Hall_log");
+  biasmuXS->AttachTo(logicHall);
+  G4LogicalVolume* logicTank = volumeStore->GetVolume("Tank_log");
+  biasmuXS->AttachTo(logicTank);
+  G4LogicalVolume* logicLar = volumeStore->GetVolume("Lar_log");
+  biasmuXS->AttachTo(logicLar);
+  G4LogicalVolume* logicCu = volumeStore->GetVolume("Copper_log");
+  biasmuXS->AttachTo(logicCu);
+  G4LogicalVolume* logicULar = volumeStore->GetVolume("ULar_log");
+  biasmuXS->AttachTo(logicULar);
+
+  // Baseline also has a water volume and cryostat
+  if(fGeometryName == "baseline")
+  {
+    G4LogicalVolume* logicWater = volumeStore->GetVolume("Water_log");
+    biasmuXS->AttachTo(logicWater);
+    G4LogicalVolume* logicCout = volumeStore->GetVolume("Cout_log");
+    biasmuXS->AttachTo(logicCout);
+    G4LogicalVolume* logicCinn = volumeStore->GetVolume("Cinn_log");
+    biasmuXS->AttachTo(logicCinn);
+    G4LogicalVolume* logicCLid = volumeStore->GetVolume("Lid_log");
+    biasmuXS->AttachTo(logicCLid);
+    G4LogicalVolume* logicCBot = volumeStore->GetVolume("Bot_log");
+    biasmuXS->AttachTo(logicCBot);
+  }
+  // Alternative has the membrane and insulator
+  else if(fGeometryName == "alternative")
+  {
+    G4LogicalVolume* logicPu = volumeStore->GetVolume("Pu_log");
+    biasmuXS->AttachTo(logicPu);
+    G4LogicalVolume* logicMembrane = volumeStore->GetVolume("Membrane_log");
+    biasmuXS->AttachTo(logicMembrane);
+  }
+}
+
+auto WLGDDetectorConstruction::SetupAlternative() -> G4VPhysicalVolume*
+{
+  // Get materials
+  auto* worldMaterial = G4Material::GetMaterial("G4_Galactic");
+  auto* larMat        = G4Material::GetMaterial("G4_lAr");
+  auto* airMat        = G4Material::GetMaterial("G4_AIR");
+  auto* steelMat      = G4Material::GetMaterial("G4_STAINLESS-STEEL");
+  auto* copperMat     = G4Material::GetMaterial("G4_Cu");
+  auto* stdRock       = G4Material::GetMaterial("StdRock");
+  auto* puMat         = G4Material::GetMaterial("polyurethane");
+  auto* roiMat        = G4Material::GetMaterial("enrGe");
 
   // size parameter, unit [cm]
   // cavern
@@ -317,14 +357,6 @@ auto WLGDDetectorConstruction::SetupAlternative() -> G4VPhysicalVolume*
                     "ULar_phys4", fLarLogical, false, 3, true);
 
   //
-  // User limits
-  //
-  //  G4double maxTime    = 1 * ms;  // affects long-lived neutrons
-  //  auto     outerlimit = new G4UserLimits(DBL_MAX, DBL_MAX, maxTime);
-  //  fCavernLogical->SetUserLimits(outerlimit);
-  //  fLarLogical->SetUserLimits(outerlimit);
-
-  //
   // Visualization attributes
   //
   fWorldLogical->SetVisAttributes(G4VisAttributes::GetInvisible());
@@ -353,40 +385,15 @@ auto WLGDDetectorConstruction::SetupAlternative() -> G4VPhysicalVolume*
 
 auto WLGDDetectorConstruction::SetupBaseline() -> G4VPhysicalVolume*
 {
-  // Materials for this geometry
-  G4Material* worldMaterial =
-    G4NistManager::Instance()->FindOrBuildMaterial("G4_Galactic");
-  G4Material* larMat   = G4NistManager::Instance()->FindOrBuildMaterial("G4_lAr");
-  G4Material* airMat   = G4NistManager::Instance()->FindOrBuildMaterial("G4_AIR");
-  G4Material* waterMat = G4NistManager::Instance()->FindOrBuildMaterial("G4_WATER");
-  G4Material* steelMat =
-    G4NistManager::Instance()->FindOrBuildMaterial("G4_STAINLESS-STEEL");
-  G4Material* copperMat = G4NistManager::Instance()->FindOrBuildMaterial("G4_Cu");
-
-  auto* C  = new G4Element("Carbon", "C", 6., 12.011 * g / mole);
-  auto* O  = new G4Element("Oxygen", "O", 8., 16.00 * g / mole);
-  auto* Ca = new G4Element("Calcium", "Ca", 20., 40.08 * g / mole);
-  auto* Mg = new G4Element("Magnesium", "Mg", 12., 24.31 * g / mole);
-
-  // Standard Rock definition, similar to Gran Sasso rock
-  // with density from PDG report
-  auto* stdRock = new G4Material("StdRock", 2.65 * g / cm3, 4);
-  stdRock->AddElement(O, 52.0 * perCent);
-  stdRock->AddElement(Ca, 27.0 * perCent);
-  stdRock->AddElement(C, 12.0 * perCent);
-  stdRock->AddElement(Mg, 9.0 * perCent);
-
-  // enriched Germanium from isotopes
-  auto* Ge_74 = new G4Isotope("Ge74", 32, 74, 74.0 * g / mole);
-  auto* Ge_76 = new G4Isotope("Ge76", 32, 76, 76.0 * g / mole);
-
-  auto* eGe = new G4Element("enriched Germanium", "enrGe", 2);
-  eGe->AddIsotope(Ge_76, 88. * perCent);
-  eGe->AddIsotope(Ge_74, 12. * perCent);
-
-  G4double density = 3.323 * mg / cm3;
-  auto*    roiMat  = new G4Material("enrGe", density, 1);
-  roiMat->AddElement(eGe, 1);
+  // Get materials
+  auto* worldMaterial = G4Material::GetMaterial("G4_Galactic");
+  auto* larMat        = G4Material::GetMaterial("G4_lAr");
+  auto* airMat        = G4Material::GetMaterial("G4_AIR");
+  auto* waterMat      = G4Material::GetMaterial("G4_WATER");
+  auto* steelMat      = G4Material::GetMaterial("G4_STAINLESS-STEEL");
+  auto* copperMat     = G4Material::GetMaterial("G4_Cu");
+  auto* stdRock       = G4Material::GetMaterial("StdRock");
+  auto* roiMat        = G4Material::GetMaterial("enrGe");
 
   // constants
   // size parameter, unit [cm]
@@ -573,15 +580,6 @@ auto WLGDDetectorConstruction::SetupBaseline() -> G4VPhysicalVolume*
 
   new G4PVPlacement(nullptr, G4ThreeVector(0., -ringrad * cm, cushift * cm), fUlarLogical,
                     "ULar_phys4", fLarLogical, false, 3, true);
-
-  //
-  // User limits
-  //
-  //  G4double maxTime    = 1 * ms;  // affects long-lived neutrons
-  //  auto     outerlimit = new G4UserLimits(DBL_MAX, DBL_MAX, maxTime);
-  //  fCavernLogical->SetUserLimits(outerlimit);
-  //  fWaterLogical->SetUserLimits(outerlimit);
-  //  fLarLogical->SetUserLimits(outerlimit);
 
   //
   // Visualization attributes
