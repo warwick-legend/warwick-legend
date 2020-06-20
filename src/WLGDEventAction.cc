48b3bf7bab83cbcbb2eda225dd3d52ddf2840606
Yorck Ramachers <y.a.ramachers@warwick.ac.uk>
2020-06-20 13:36:10 +0100

    [event] filter event output on action in germanium, no git in Ge, no output.

--- src/WLGDEventAction.cc
+++ src/WLGDEventAction.cc
@@ -106,6 +106,9 @@ void WLGDEventAction::EndOfEventAction(const G4Event* event)
   G4THitsMap<G4double>*      HitsMap  = GetHitsCollection(fEdepID, event);
   G4THitsMap<G4ThreeVector>* LocMap   = GetVecHitsCollection(fLocID, event);
   G4THitsMap<G4double>*      TimeMap  = GetHitsCollection(fTimeID, event);
+ 
+  if (THitsMap->entries()<=0)
+    return;   // no action on no event in Germanium
 
   // get analysis manager
   auto analysisManager = G4AnalysisManager::Instance();
