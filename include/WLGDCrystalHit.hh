3db613373c79a1520d5601e51fd6a99119a0888e
Yorck Ramachers <y.a.ramachers@warwick.ac.uk>
2021-04-27 15:04:10 +0100

    [scorer] change scoring to SD, Hit model.

---
+++ include/WLGDCrystalHit.hh
@@ -0,0 +1,80 @@
+#ifndef WLGDCrystalHit_h
+#define WLGDCrystalHit_h 1
+
+#include "G4VHit.hh"
+#include "G4THitsCollection.hh"
+#include "G4Allocator.hh"
+#include "G4ThreeVector.hh"
+
+/// Crystal hit class
+///
+/// It defines data members to store the energy deposit,
+/// and position in a selected volume:
+
+class WLGDCrystalHit : public G4VHit
+{
+  public:
+    WLGDCrystalHit();
+    WLGDCrystalHit(const WLGDCrystalHit&);
+    virtual ~WLGDCrystalHit();
+
+    // operators
+    const WLGDCrystalHit& operator=(const WLGDCrystalHit&);
+    G4bool operator==(const WLGDCrystalHit&) const;
+
+    inline void* operator new(size_t);
+    inline void  operator delete(void*);
+
+    // methods from base class
+    virtual void Draw();
+    virtual void Print();
+
+    // Set methods
+    void SetTID      (G4int   tid)      { fTid    = tid; };
+    void SetTime     (G4double ti)      { fTime   = ti; };
+    void SetWeight   (G4double we)      { fWeight = we; };
+    void SetEdep     (G4double de)      { fEdep   = de; };
+    void SetPos      (G4ThreeVector xyz){ fPos    = xyz; };
+
+    // Get methods
+    G4int    GetTID()  const     { return fTid; };
+    G4double GetTime() const     { return fTime; };
+    G4double GetWeight() const   { return fWeight; };
+    G4double GetEdep() const     { return fEdep; };
+    G4ThreeVector GetPos() const { return fPos; };
+
+  private:
+
+      G4int         fTid;
+      G4double      fTime;
+      G4double      fWeight;
+      G4double      fEdep;
+      G4ThreeVector fPos;
+};
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+typedef G4THitsCollection<WLGDCrystalHit> WLGDCrystalHitsCollection;
+
+extern G4ThreadLocal G4Allocator<WLGDCrystalHit>* WLGDCrystalHitAllocator;
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+inline void* WLGDCrystalHit::operator new(size_t)
+{
+  if(!WLGDCrystalHitAllocator)
+      WLGDCrystalHitAllocator = new G4Allocator<WLGDCrystalHit>;
+  return (void *) WLGDCrystalHitAllocator->MallocSingle();
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+inline void WLGDCrystalHit::operator delete(void *hit)
+{
+  WLGDCrystalHitAllocator->FreeSingle((WLGDCrystalHit*) hit);
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+#endif
+
---
+++ src/WLGDCrystalHit.cc
@@ -0,0 +1,57 @@
+#include "WLGDCrystalHit.hh"
+
+G4ThreadLocal G4Allocator<WLGDCrystalHit>* WLGDCrystalHitAllocator=0;
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+WLGDCrystalHit::WLGDCrystalHit()
+ : G4VHit(),
+   fTID(0),
+   fTime(0.),
+   fWeight(0.),
+   fEdep(0.),
+   fPos(G4ThreeVector())
+{}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+WLGDCrystalHit::~WLGDCrystalHit() {}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+WLGDCrystalHit::WLGDCrystalHit(const WLGDCrystalHit& right)
+  : G4VHit()
+{
+  fTID       = right.fTID;
+  fTime      = right.fTime;
+  fWeight    = right.fWeight;
+  fEdep      = right.fEdep;
+  fPos       = right.fPos;
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+const WLGDCrystalHit& WLGDCrystalHit::operator=(const WLGDCrystalHit& right)
+{
+  fTID       = right.fTID;
+  fTime      = right.fTime;
+  fWeight    = right.fWeight;
+  fEdep      = right.fEdep;
+  fPos       = right.fPos;
+
+  return *this;
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+G4bool WLGDCrystalHit::operator==(const WLGDCrystalHit& right) const
+{
+  return ( this == &right ) ? true : false;
+}
+
+void WLGDCrystalHit::Draw()
+{ ; }
+
+void WLGDCrystalHit::Print()
+{ ; }
+
