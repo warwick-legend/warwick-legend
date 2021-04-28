22efff769d7ce9cddf004d6ae13201a2c085edd9
Yorck Ramachers <y.a.ramachers@warwick.ac.uk>
2021-04-28 10:09:14 +0100

    [tinfo] removed: no tracking information required.

--- src/WLGDStackingAction.cc
+++ src/WLGDStackingAction.cc
@@ -1,15 +1,15 @@
 #include "WLGDStackingAction.hh"
-#include "WLGDTrackInformation.hh"
+// #include "WLGDTrackInformation.hh"
 
 G4ClassificationOfNewTrack WLGDStackingAction ::ClassifyNewTrack(const G4Track* aTrack)
 {
   G4ClassificationOfNewTrack classification = fUrgent;
 
-  if(aTrack->GetParentID() == 0)  // Primary particle
-  {
-    WLGDTrackInformation* trackInfo = new WLGDTrackInformation(aTrack);
-    aTrack->SetUserInformation(trackInfo);  // attach own track info
-  }
+//  if(aTrack->GetParentID() == 0)  // Primary particle
+//  {
+//    WLGDTrackInformation* trackInfo = new WLGDTrackInformation(aTrack);
+//    aTrack->SetUserInformation(trackInfo);  // attach own track info
+//  }
   return classification;
 }
 
