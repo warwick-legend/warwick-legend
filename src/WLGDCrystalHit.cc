97a59bf89ddcb545d40cb3ce301e73534a33b5b7
yramachers <yramachers@users.noreply.github.com>
2021-05-13 16:11:52 +0100

    Update src/WLGDCrystalHit.cc
    Co-authored-by: Ben Morgan <drbenmorgan@users.noreply.github.com>

--- src/WLGDCrystalHit.cc
+++ src/WLGDCrystalHit.cc
@@ -2,45 +2,6 @@
 
 G4ThreadLocal G4Allocator<WLGDCrystalHit>* WLGDCrystalHitAllocator=0;
 
-//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
-
-WLGDCrystalHit::WLGDCrystalHit()
- : G4VHit(),
-   fTid(0),
-   fTime(0.),
-   fWeight(0.),
-   fEdep(0.),
-   fPos(G4ThreeVector())
-{}
-
-//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
-
-WLGDCrystalHit::~WLGDCrystalHit() {}
-
-//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
-
-WLGDCrystalHit::WLGDCrystalHit(const WLGDCrystalHit& right)
-  : G4VHit()
-{
-  fTid       = right.fTid;
-  fTime      = right.fTime;
-  fWeight    = right.fWeight;
-  fEdep      = right.fEdep;
-  fPos       = right.fPos;
-}
-
-//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
-
-const WLGDCrystalHit& WLGDCrystalHit::operator=(const WLGDCrystalHit& right)
-{
-  fTid       = right.fTid;
-  fTime      = right.fTime;
-  fWeight    = right.fWeight;
-  fEdep      = right.fEdep;
-  fPos       = right.fPos;
-
-  return *this;
-}
 
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 
@@ -54,4 +15,3 @@ void WLGDCrystalHit::Draw()
 
 void WLGDCrystalHit::Print()
 { ; }
-
