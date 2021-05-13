65ede68a990139e91262484648266951359f2093
yramachers <yramachers@users.noreply.github.com>
2021-05-13 16:06:00 +0100

    Update src/WLGDStackingAction.cc
    Co-authored-by: Ben Morgan <drbenmorgan@users.noreply.github.com>

--- src/WLGDStackingAction.cc
+++ src/WLGDStackingAction.cc
@@ -5,11 +5,6 @@ G4ClassificationOfNewTrack WLGDStackingAction ::ClassifyNewTrack(const G4Track* 
 {
   G4ClassificationOfNewTrack classification = fUrgent;
 
-//  if(aTrack->GetParentID() == 0)  // Primary particle
-//  {
-//    WLGDTrackInformation* trackInfo = new WLGDTrackInformation(aTrack);
-//    aTrack->SetUserInformation(trackInfo);  // attach own track info
-//  }
   return classification;
 }
 
